d421c882786ea5df3157b423085ef4767845d932
loulei <constantlike@gmail.com>
2019-05-17 17:13:19 +0800

    test

--- src/vm.c
+++ src/vm.c
@@ -52,8 +52,23 @@ enum {
 	FL_NEG = 1 << 2, /* N */
 };
 
+enum {
+	TRAP_GETC = 0x20, // get character from keyboard, not echoed onto the terminal
+	TRAP_OUT = 0x21,     // output a character
+	TRAP_PUTS = 0x22,     // output a word string
+	TRAP_IN = 0x23,     // get character from keyboard, echoed onto the terminal
+	TRAP_PUTSP = 0x24,     // output a byte string
+	TRAP_HALT = 0x25,     // halt the program
+};
+
+enum {
+    MR_KBSR = 0xFE00,       // keyboard status
+    MR_KBDR = 0xFE02        // keyboard data
+};
+
 uint16_t memory[UINT16_MAX];
 uint16_t regs[R_COUNT];
+int running = 1;
 
 uint16_t sign_extend(uint16_t x, int bit_count) {
 	if ((x >> (bit_count - 1)) & 1) {
@@ -73,7 +88,26 @@ void update_flags(uint16_t r) {
 	}
 }
 
+uint16_t check_key(){
+	fd_set readfds;
+	FD_ZERO(&readfds);
+	FD_SET(STDIN_FILENO, &readfds);
+
+	struct timeval timeout;
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 0;
+	return select(STDIN_FILENO+1, &readfds, NULL, NULL, &timeout) != 0;
+}
+
 uint16_t mem_read(uint16_t address) {
+	if(address == MR_KBSR){
+		if(check_key()){
+			memory[MR_KBSR] = (1 << 15);
+			memory[MR_KBDR] = getchar();
+		}else{
+			memory[MR_KBSR] = 0;
+		}
+	}
 	return memory[address];
 }
 
@@ -196,7 +230,61 @@ void ins(uint16_t instr) {
 		mem_write(regs[base_r] + pc_offset, sr);
 	}
 		break;
+	case OP_TRAP: {
+		uint16_t trap_vect = instr & 0xFF;
+		switch (trap_vect) {
+		case TRAP_GETC: {
+			regs[R_R0] = (uint16_t) getchar();
+		}
+			break;
+		case TRAP_OUT: {
+			putc((char) regs[R_R0], stdout);
+			fflush(stdout);
+		}
+			break;
+		case TRAP_PUTS: {
+			uint16_t *c = memory + regs[R_R0];
+			while (*c) {
+				putc((char) *c, stdout);
+				++c;
+			}
+			fflush(stdout);
+		}
+			break;
+		case TRAP_IN: {
+			printf("Enter a character:");
+			char c = getchar();
+			putc(c, stdout);
+			regs[R_R0] = (uint16_t) c;
+		}
+			break;
+		case TRAP_PUTSP: {
+			uint16_t *c = memory + regs[R_R0];
+			while (*c) {
+				char cha = (*c) & 0xFF;
+				putc(cha, stdout);
+				char cha1 = (*c) >> 8;
+				if (cha1) {
+					putc(cha1, stdout);
+				}
+				c++;
+			}
+			fflush(stdout);
+		}
+			break;
+		case TRAP_HALT: {
+			puts("HALT");
+			fflush(stdout);
+			running = 0;
+		}
+			break;
+		default:
+			break;
+		}
+	}
+		break;
 	default:
+		abort();
 		break;
 	}
 }
@@ -216,7 +304,56 @@ void output_assert(int expression, const char *s) {
 	printf("%s\n", s);
 }
 
-int main(void) {
+uint16_t swap16(uint16_t x){
+	return (x << 8) | (x >> 8);
+}
+
+void read_image_file(FILE *file){
+	uint16_t origin;
+	fread(&origin, sizeof(origin), 1, file);
+	origin = swap16(origin);
+
+	uint16_t max_read = UINT16_MAX - origin;
+	uint16_t *p = memory + origin;
+	size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+	while(read-- > 0){
+		*p = swap16(*p);
+		++p;
+	}
+}
+
+int read_image(const char *image_path){
+	FILE *file = fopen(image_path, "rb");
+	if(!file)
+		return 0;
+	read_image_file(file);
+	fclose(file);
+	return 1;
+}
+
+
+
+struct termios original_tio;
+
+void disable_input_buffering(){
+	tcgetattr(STDIN_FILENO, &original_tio);
+	struct termios new_tio = original_tio;
+	new_tio.c_lflag &= ~ICANON & ~ECHO;
+	tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering(){
+	tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+void handle_interrupt(int signal){
+	restore_input_buffering();
+	printf("\n");
+	exit(-2);
+}
+
+int test() {
 
 	uint16_t instr;
 	printf("¿ªÊ¼²âÊÔ£º\n");
@@ -344,5 +481,31 @@ int main(void) {
 		output_assert(regs[7] == mem_read(add), "OP_STR ¡Ì");
 		reset();
 	}
+	return EXIT_SUCCESS;
+}
+
+
+int main(int argc, char **argv) {
+//	test();
+
+	read_image(argv[1]);
+
+	signal(SIGINT, handle_interrupt);
+	disable_input_buffering();
+
+	enum{
+		PC_START = 0x3000
+	};
+	regs[R_PC] = PC_START;
+
+//	int count = 100;
+	while(running){
+		uint16_t instr = mem_read(regs[R_PC]++);
+		ins(instr);
+//		uint16_t op = instr >> 12;
+	}
+
+	restore_input_buffering();
+
 	return EXIT_SUCCESS;
 }
